d9b77d7f156d8bd676c110398454c4de08289741
Yegor Gladkov <gladkovyegor@gmail.com>
2019-07-27 00:10:57 +0400

    Added classes.

--- tests.py
+++ tests.py
@@ -1,50 +1,56 @@
 from unittest import TestCase
-from Caesar import encrypt, encrypt_modular, decrypt_modular
+from Caesar import Caesar
 
 
 class CaesarTestCase(TestCase):
+    def setUp(self) -> None:
+        self.c = Caesar()
+
+    def tearDown(self) -> None:
+        del self.c
+
     def test_encrypt(self):
-        self.assertEqual(encrypt("Чу, я слышу пушек гром!", 3), "Ъц, в фоюыц тцызн ёусп!")
-        self.assertEqual(encrypt("Hello world!", 15, 'eng'), "Wtaad ldgas!")
-        self.assertEqual(encrypt("Hello world!", 15, 'rus'), "Hello world!")
-        self.assertEqual(encrypt("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
-        self.assertEqual(encrypt("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
-        self.assertEqual(encrypt("Oops!", 0), "Oops!")
-        self.assertEqual(encrypt("Упс!", 0), "Упс!")
-        self.assertEqual(encrypt("", 10, 'rus'), "")
-        self.assertEqual(encrypt("", 10, 'eng'), "")
+        self.assertEqual(self.c.encrypt("Чу, я слышу пушек гром!", 3), "Ъц, в фоюыц тцызн ёусп!")
+        self.assertEqual(self.c.encrypt("Hello world!", 15, 'eng'), "Wtaad ldgas!")
+        self.assertEqual(self.c.encrypt("Hello world!", 15, 'rus'), "Hello world!")
+        self.assertEqual(self.c.encrypt("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
+        self.assertEqual(self.c.encrypt("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
+        self.assertEqual(self.c.encrypt("Oops!", 0), "Oops!")
+        self.assertEqual(self.c.encrypt("Упс!", 0), "Упс!")
+        self.assertEqual(self.c.encrypt("", 10, 'rus'), "")
+        self.assertEqual(self.c.encrypt("", 10, 'eng'), "")
         self.assertEqual(
-            encrypt("Чу, я слышу пушек гром!", 100, 'rus'),
+            self.c.encrypt("Чу, я слышу пушек гром!", 100, 'rus'),
             "Shift greater than the number of letters of the alphabet!")
-        self.assertEqual(encrypt("Hello world!", -3, 'eng'), encrypt("Hello world!", 23, 'eng'))
-        self.assertEqual(encrypt("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
+        self.assertEqual(self.c.encrypt("Hello world!", -3, 'eng'), self.c.encrypt("Hello world!", 23, 'eng'))
+        self.assertEqual(self.c.encrypt("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
 
     def test_encrypt_modular(self):
-        self.assertEqual(encrypt_modular("Чу, я слышу пушек гром!", 3), "Ъц, в фоюыц тцызн ёусп!")
-        self.assertEqual(encrypt_modular("Hello world!", 15, 'eng'), "Wtaad ldgas!")
-        self.assertEqual(encrypt_modular("Hello world!", 15, 'rus'), "Hello world!")
-        self.assertEqual(encrypt_modular("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
-        self.assertEqual(encrypt_modular("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
-        self.assertEqual(encrypt_modular("Oops!", 0), "Oops!")
-        self.assertEqual(encrypt_modular("Упс!", 0), "Упс!")
-        self.assertEqual(encrypt_modular("", 10, 'rus'), "")
-        self.assertEqual(encrypt_modular("", 10, 'eng'), "")
-        self.assertEqual(encrypt_modular("Чу, я слышу пушек гром!", 100, 'rus'), "Шф, а тмьщф рфщёл дспн!")
-        self.assertEqual(encrypt_modular("Hello world!", -3, 'eng'), encrypt_modular("Hello world!", 23, 'eng'))
-        self.assertEqual(encrypt_modular("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
+        self.assertEqual(self.c.encrypt_modular("Чу, я слышу пушек гром!", 3), "Ъц, в фоюыц тцызн ёусп!")
+        self.assertEqual(self.c.encrypt_modular("Hello world!", 15, 'eng'), "Wtaad ldgas!")
+        self.assertEqual(self.c.encrypt_modular("Hello world!", 15, 'rus'), "Hello world!")
+        self.assertEqual(self.c.encrypt_modular("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
+        self.assertEqual(self.c.encrypt_modular("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
+        self.assertEqual(self.c.encrypt_modular("Oops!", 0), "Oops!")
+        self.assertEqual(self.c.encrypt_modular("Упс!", 0), "Упс!")
+        self.assertEqual(self.c.encrypt_modular("", 10, 'rus'), "")
+        self.assertEqual(self.c.encrypt_modular("", 10, 'eng'), "")
+        self.assertEqual(self.c.encrypt_modular("Чу, я слышу пушек гром!", 100, 'rus'), "Шф, а тмьщф рфщёл дспн!")
+        self.assertEqual(self.c.encrypt_modular("Hello world!", -3, 'eng'), self.c.encrypt_modular("Hello world!", 23, 'eng'))
+        self.assertEqual(self.c.encrypt_modular("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
 
     def test_decrypt_modular(self):
-        self.assertEqual(decrypt_modular("Ъц, в фоюыц тцызн ёусп!", 3), "Чу, я слышу пушек гром!")
-        self.assertEqual(decrypt_modular("Wtaad ldgas!", 15, 'eng'), "Hello world!")
-        self.assertEqual(decrypt_modular("Hello world!", 15, 'rus'), "Hello world!")
-        self.assertEqual(decrypt_modular("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
-        self.assertEqual(decrypt_modular("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
-        self.assertEqual(decrypt_modular("Oops!", 0), "Oops!")
-        self.assertEqual(decrypt_modular("Упс!", 0), "Упс!")
-        self.assertEqual(decrypt_modular("", 10, 'rus'), "")
-        self.assertEqual(decrypt_modular("", 10, 'eng'), "")
-        self.assertEqual(decrypt_modular("Чу, я слышу пушек гром!", 100, 'rus'), "Цт, ю ркъчт отчдй впнл!")
-        self.assertEqual(decrypt_modular("Hello world!", -3, 'eng'), decrypt_modular("Hello world!", 23, 'eng'))
-        self.assertEqual(decrypt_modular("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
+        self.assertEqual(self.c.decrypt_modular("Ъц, в фоюыц тцызн ёусп!", 3), "Чу, я слышу пушек гром!")
+        self.assertEqual(self.c.decrypt_modular("Wtaad ldgas!", 15, 'eng'), "Hello world!")
+        self.assertEqual(self.c.decrypt_modular("Hello world!", 15, 'rus'), "Hello world!")
+        self.assertEqual(self.c.decrypt_modular("Чу, я слышу пушек гром!", 15, 'eng'), "Чу, я слышу пушек гром!")
+        self.assertEqual(self.c.decrypt_modular("Ви говорите англійською?", 1, 'ukr'), "Selected language is not supported!")
+        self.assertEqual(self.c.decrypt_modular("Oops!", 0), "Oops!")
+        self.assertEqual(self.c.decrypt_modular("Упс!", 0), "Упс!")
+        self.assertEqual(self.c.decrypt_modular("", 10, 'rus'), "")
+        self.assertEqual(self.c.decrypt_modular("", 10, 'eng'), "")
+        self.assertEqual(self.c.decrypt_modular("Чу, я слышу пушек гром!", 100, 'rus'), "Цт, ю ркъчт отчдй впнл!")
+        self.assertEqual(self.c.decrypt_modular("Hello world!", -3, 'eng'), self.c.decrypt_modular("Hello world!", 23, 'eng'))
+        self.assertEqual(self.c.decrypt_modular("Hello world!", 0.15, 'eng'), "Shift is not integer number!")
 
 
